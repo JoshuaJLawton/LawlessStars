65e8d62d0a5b36e4e3a178974c7e0965745fe621
Joshua Lawton <JJJLawton@outlook.com>
2019-03-26 19:31:00 +0000

    Prototype v1.2.1
    Stars Added

---
+++ LS/Assets/Scripts/Controllers/StarController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarController : MonoBehaviour {
+
+    private Animator Anim;
+    private int Rand;
+    
+	// Use this for initialization
+	void Start ()
+    {
+        Anim = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+            //Anim.Play("Star", 0, 0);
+        //}
+        StartCoroutine(Twinkle());
+    }
+
+    IEnumerator Twinkle()
+    {
+        Rand = Random.Range(3, 10);
+        yield return new WaitForSeconds(Rand);
+        Anim.Play("Star", 0, 0);
+    }
+}
